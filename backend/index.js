0afed68145982731870587782413f8df2633dda5
Eidan <Eidandreammer@gmail.com>
2025-11-26 19:42:22 -0500

    Inicio del backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
-import express from "express";
-import cors from "cors";
-import { pool } from "pg";
+//Importar librerias
+const express = require("express");
+const cors = require("cors");
+const { pool } = require("pg");
 
+//Crear la app de express
 const app = express();
+
+//Configuracion basica
 const port = 4000;
 
-app.use(cors());
-app.use(express.json());
+//Middlewares
+app.use(cors()); //Permite que react pueda llamar a este servidor
+app.use(express.json()); //Permite leer JSON en req.body
 
+//Configurar conexion a PostgreSQL
 const pool = new pool({
   user: "postgres",
   host: "localhost",
@@ -15,3 +21,19 @@ const pool = new pool({
   password: "000",
   port: 5432,
 });
+
+//Ruta de read (Obtener todos los items)
+//Get /api/items
+app.get("/api/items", async (req, res) => {
+  try {
+    const result = await pool.query(
+      "select * from usuarios order by idusuarios asc"
+    );
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error al obtener usuarios: ", error);
+    res.status(500).json({ error: "Error al obtener usuarios" });
+  }
+});
+
+
